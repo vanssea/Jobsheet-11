5a4a8c87b7b0262791fde9ee77f93273ea2e8d69
vanessacristin <cristinvanessa6@gmail.com>
2023-11-09 12:25:26 +0700

    Modifikasi

--- NestedLoop_2341720026.java
+++ NestedLoop_2341720026.java
@@ -4,21 +4,17 @@ public class NestedLoop_2341720026 {
         Scanner scanner = new Scanner(System.in);
 
         double temps [][] = new double [5][7];
+        double [] kotaRata2 = new double[5];
 
         for (int i = 0; i < temps.length; i++) {
             System.out.println("Kota ke-" + i);
+            double kotaJum = 0;
             for (int j = 0; j < temps[0].length; j++) {
                 System.out.print("Hari ke-" + (j + 1) + ": ");
                 temps[i][j] = scanner.nextDouble();
+                kotaJum += temps[i][j];
             }
-            System.out.println();
-        }
-
-        for (int i = 0; i < temps.length; i++) {
-            System.out.print("Kota ke-" + (i + 1) + ": ");
-            for (int j = 0; j < temps[0].length; j++) {
-                System.out.print(temps[i][j] + " ");
-            }
+            kotaRata2[i] = kotaJum / temps[0].length;
             System.out.println();
         }
 
@@ -27,6 +23,8 @@ public class NestedLoop_2341720026 {
             for (double temp : temps[i]) {
                 System.out.print(temp + " ");
             }
+            System.out.println("Rata - rata suhu : " + kotaRata2[i]);
+            System.out.println();
         }
     }
 }
